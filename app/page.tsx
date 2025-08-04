8ddbf1ff60efb2cea2a1a88a0727ee14b377aa08
ahmetselim55 <136008521+ahmetselim55@users.noreply.github.com>
2025-08-04 17:54:29 +0300

    Add portfolio component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import BarisOzcanPortfolio from '@/components/BarisOzcanPortfolio'
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Test Page</h1>
-      <p>Bu sayfa çalışıyor mu?</p>
-    </div>
-  );
+  return <BarisOzcanPortfolio />
 }
+
 
